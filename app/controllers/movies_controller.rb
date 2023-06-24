77548d6f30ee273eb961587b432d524b7ec89f24
malcomowilla <owillamalcom@gmail.com>
2023-06-24 22:32:57 +0300

    solved

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,17 @@
 class MoviesController < ApplicationController
-  
+  def create
+    movie = Movie.create(bird_params)
+    render json: movie
+  end
   def index
     movies = Movie.all
     render json: movies
   end
+private
+  def bird_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
+
 
 end
